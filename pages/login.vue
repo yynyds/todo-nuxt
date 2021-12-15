bc18d3077220f47c1513476606c3e3682308aad7
yynyds <yarik1120@gmail.com>
2021-12-15 13:09:43 +0200

    add empty layout and login page

---
+++ pages/login.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <form @submit.prevent="onSubmit">
+      <h1>Login page</h1>
+      <hr>
+      <div class="form-group">
+        <input type="text" class="form-control">
+      </div>
+
+      <p>
+        <nuxt-link to="/">To home page</nuxt-link>
+      </p>
+
+      <button class="btn btn-primary" type="submit">Login</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  layout: 'empty',
+  methods: {
+    onSubmit() {
+      console.log('onSubmit')
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  width: 500px;
+  margin: 0 auto;
+}
+</style>
