a40d36f2c1ed6e66c9e7ad6e39f8fe87ba75e5d8
yynyds <yarik1120@gmail.com>
2021-12-16 13:04:32 +0200

    add user fields

--- pages/todos/_id.vue
+++ pages/todos/_id.vue
@@ -1,12 +1,38 @@
 <template>
   <section>
-    <h1>{{user.name}}</h1>
+    <h1>Additional info about user</h1>
     <hr>
+    <form class="row g-3">
+      <div class="col-auto">
+        <label for="inputName" class="visually-hidden">Name</label>
+        <input id="inputName" class="form-control" type="text" placeholder="Name" v-model="user.name">
+      </div>
+      <div class="col-auto">
+        <label for="inputUsername" class="visually-hidden">Username</label>
+        <input id="inputUsername" class="form-control" type="text" placeholder="Username" v-model="user.username">
+      </div>
+      <div class="col-auto">
+        <label for="inputEmail" class="visually-hidden">Email</label>
+        <input id="inputEmail" class="form-control" type="email" placeholder="Email" v-model="user.email">
+      </div>
+      <div class="col-auto">
+        <label for="inputWebsite" class="visually-hidden">Website</label>
+        <input id="inputWebsite" class="form-control" type="text" placeholder="Website" v-model="user.website">
+      </div>
+      <div class="col-auto">
+        <label for="inputCompanyName" class="visually-hidden">Company name</label>
+        <input id="inputCompanyName" class="form-control" type="text" placeholder="Comp. name" v-model="user.company.name">
+      </div>
+      <div class="col-auto" style="align-self: end;">
+        <button type="submit" class="btn btn-primary">Confirm identity</button>
+      </div>
+    </form>
   </section>
 </template>
 
 <script>
 export default {
+  middleware: ['auth'],
   validate({params}) {
     return /^\d+$/.test(params.id)
   },
