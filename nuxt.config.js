48ecc6a5b1079ae27f9b396265c80c7424d741cc
yynyds <yarik1120@gmail.com>
2022-01-01 23:52:45 +0200

    upd

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,6 +19,10 @@ export default {
   ** Customize the progress-bar color
   */
   loading: { color: 'blue' },
+  target: 'static',
+  router: {
+    base: '/todo-nuxt/'
+  },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
